8f73094ecd83b8bc9bc960ffefff80598d0da365
Kah Hou Teo <khteo@nus.edu.sg>
2013-03-30 20:43:16 +0800

    SimpleGA: some formatting changes.

--- SimpleGA.java
+++ SimpleGA.java
@@ -1,51 +1,47 @@
 import java.util.*;
 
-
 /**
- * Simplest genetic algorithm possible
- * 
+ * The simplest genetic algorithm possible.
+ *
  * This algorithm creates a genetic algorithm with the following properties:
  * 1) Constant population size
  * 2) Flexible maximum height (set to infinity if we want to simulate full games)
  * 3) Crossover rate refers to the fraction of individuals which will be formed by child chromosomes
  * 4) Mutation rate refers to the fraction of *CHILDREN* which will be mutated
- * 5) We test each chromsome with 5 games per generation.
- * 6) Chromosomes to be chosen as parents on a weighted basis - chromosomes with a higher fitness are 
+ * 5) We test each chromosome with 5 games per generation.
+ * 6) Chromosomes to be chosen as parents on a weighted basis - chromosomes with a higher fitness are
  * more likely to be chosen
- * 7) The same chromosome may be chosen to mate with itself - no restricions on that.
- * 
+ * 7) The same chromosome may be chosen to mate with itself - no restrictionss on that.
+ *
  * This initial population should be seeded by the user, and all chromosomes are assumed
  * to be have relevant crossover/mutation operators defined. If there are different classes
  * of chromosomes available, then there must be crossover operations defined for both of them (wrt each other)
- * 
- * 
- * 
  */
-public class SimpleGA extends GeneticAlgo{
+public class SimpleGA extends GeneticAlgo {
 
-	private double mutationProb;
-	private double crossoverRate;
-	private int maxHeight;
-	
+    private int maxHeight;
+    private double mutationProb;
+    private double crossoverRate;
+
     /**
-     * @param initialPop
-     * Initial population. Also determines the population size
-     * @param maxHeight
-     * Maxmimum height to use in simulations. Set to a low value to speed up simulation time at the expense of accuracy
-     * @param mutationProb
-     * Double ranging from 0-1 inclusive. Set to 0 if we do not want mutations, and 1, if we want all children to be mutated
-     * @param crossoverRate
-     * Double ranging from 0-1 inclusive. Set to 0 if we do not want to population to evolve (don't do this), or 1 if we want each 
-     * generation to completely replace its parents. Bear in in mind that high crossover rates may result in the loss of possibly good genes.
-     * It may also result in longer times for each generation.
+     * @param initialPop    Initial population. It also determines the population size.
+     * @param maxHeight     Maximum height to use in simulations. Set to a low value to speed up simulation time at the
+     *                      expense of accuracy.
+     * @param mutationProb  Double ranging from 0-1 inclusive. Set to 0 if we do not want mutations, and 1, if we want
+     *                      all children to be mutated.
+     * @param crossoverRate Double ranging from 0-1 inclusive. Set to 0 if we do not want to population to evolve (don't
+     *                      do this), or 1 if we want each generation to completely replace its parents. Bear in in mind
+     *                      that high crossover rates may result in the loss of possibly good genes. It may also result
+     *                      in longer times for each generation.
      */
     SimpleGA(List<Chromosome> initialPop, int maxHeight, double mutationProb, double crossoverRate) {
     	super(initialPop);
-    	this.mutationProb = mutationProb;
-    	this.crossoverRate = crossoverRate;
-    	this.maxHeight = maxHeight;
+        this.maxHeight = maxHeight;
+        this.mutationProb = mutationProb;
+        this.crossoverRate = crossoverRate;
     }
-     
+
+    @Override
     protected double computeFitnessLevel(Chromosome c) {
         // Run simulation 5 times, compute the number of lines removed, and take average
         for (int x = 0; x < 5; x++) {
@@ -55,6 +51,7 @@ public class SimpleGA extends GeneticAlgo{
         return c.getFitness();
     }
 
+    @Override
     protected List<ParentPair> selection() {
         List<ParentPair> ret = new ArrayList<ParentPair>();
         int numChildren = (int)((population.size()) * crossoverRate);
@@ -84,6 +81,7 @@ public class SimpleGA extends GeneticAlgo{
         return ret;
     }
 
+    @Override
     protected List<Chromosome> acceptance(List<Chromosome> children) {
         // To maintain the population size, we remove the weakest parents.
     	int numParentsRequired = population.size() - children.size();
@@ -99,6 +97,7 @@ public class SimpleGA extends GeneticAlgo{
     	return children;
     }
 
+    @Override
     protected List<Chromosome> mutate(List<Chromosome> children) {
     	Random rng = new Random();
     	for (Chromosome c : children) {
@@ -109,6 +108,7 @@ public class SimpleGA extends GeneticAlgo{
         return children;
     }
 
+    @Override
     protected List<Chromosome> crossover(List<ParentPair> parents) {
         List<Chromosome> ret = new ArrayList<Chromosome>();
         for (ParentPair p : parents) {
@@ -120,14 +120,14 @@ public class SimpleGA extends GeneticAlgo{
 	@Override
 	public String toString() {
     	StringBuilder str = new StringBuilder();
-    	str.append("PopSize " + population.size()+"\n");
-        str.append("BestFitness " + getBest().getFitness()+"\n");
-        str.append("Parameters:\n" + getBest().toString()+"\n");
-        
+    	str.append("PopSize " + population.size() + "\n");
+        str.append("BestFitness " + getBest().getFitness() + "\n");
+        str.append("Parameters:\n" + getBest().toString() + "\n");
+
         List<Chromosome> pop = population;
         str.append("POPULATION\n");
         for (Chromosome c : pop) {
-        	str.append(c.toString()+"\n");
+        	str.append(c.toString() + "\n");
         }
         return str.toString();
 	}
